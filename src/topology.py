4a99bd64e8ac132f13659d25d8aa32cd129a7e09
Fabio Frazao <fsfrazao@gmail.com>
2018-01-08 13:14:55 -0330

    autopep8

--- src/topology.py
+++ src/topology.py
@@ -6,6 +6,7 @@ from tables import *
 import json
 import numpy as np
 
+
 class Tree_Grid(Rectangular_Grid):
     """ Represent space as a grid.
 
@@ -31,32 +32,29 @@ class Tree_Grid(Rectangular_Grid):
 
     """
 
-    def __init__(self,x_max,y_max,patch_area,I0,k,delta_h,phi_act,lday,ndim=5,dim_names=["GroundLight","LianaCoverage","Temperature","WindSpeed","Humidity"]):
-        super().__init__(x_max,y_max,ndim=ndim,dim_names=dim_names)
-        self.patch_area=patch_area
-        self.delta_h=delta_h
-        self.I0=I0
-        self.k=k
-        self.lday=lday
-        self.phi_act=phi_act
-        self.LAIs={}
-        self.CCAs={}
-        self.total_area=self.x_max * self.y_max *self.patch_area/10000
-        #s={}
-        self.trees_per_patch={(x,y):[] for x in range(0,self.x_max) for y in range(0,self.y_max)}
-        #self.FTs=["FT1","FT2","FT3","FT4","FT5","FT6"]
-        self.FTs=[k for k in Tree.DERIVED_TREES.keys()]
-        self.seedbank={k:[] for k in self.FTs}
-
-
-
-
+    def __init__(self, x_max, y_max, patch_area, I0, k, delta_h, phi_act, lday, ndim=5, dim_names=["GroundLight", "LianaCoverage", "Temperature", "WindSpeed", "Humidity"]):
+        super().__init__(x_max, y_max, ndim=ndim, dim_names=dim_names)
+        self.patch_area = patch_area
+        self.delta_h = delta_h
+        self.I0 = I0
+        self.k = k
+        self.lday = lday
+        self.phi_act = phi_act
+        self.LAIs = {}
+        self.CCAs = {}
+        self.total_area = self.x_max * self.y_max * self.patch_area / 10000
+        # s={}
+        self.trees_per_patch = {(x, y): [] for x in range(
+            0, self.x_max) for y in range(0, self.y_max)}
+        # self.FTs=["FT1","FT2","FT3","FT4","FT5","FT6"]
+        self.FTs = [k for k in Tree.DERIVED_TREES.keys()]
+        self.seedbank = {k: [] for k in self.FTs}
 
     def exeeding_CCA(self):
-        patches=[patch for patch,layers in self.CCAs.items() if any(layer>self.patch_area for layer in layers )]
+        patches = [patch for patch, layers in self.CCAs.items() if any(
+            layer > self.patch_area for layer in layers)]
 
         return patches
-
 
     def update(self):
         """ Execute the methods that calculate values for each dimension of the grid.
@@ -66,12 +64,13 @@ class Tree_Grid(Rectangular_Grid):
 
         """
 
-        self.LAIs=self.patch_based_LAI(Tree.TreesPerPatch())
-        self.CCAs=self.patch_based_CCA(Tree.TreesPerPatch())
+        self.LAIs = self.patch_based_LAI(Tree.TreesPerPatch())
+        self.CCAs = self.patch_based_CCA(Tree.TreesPerPatch())
         self.ground_level_light()
-        self.surface[self.dim_names["LianaCoverage"]]=self.calculate_liana_coverage()
+        self.surface[self.dim_names["LianaCoverage"]
+                     ] = self.calculate_liana_coverage()
 
-    def seed_establishment(self,seedbank):
+    def seed_establishment(self, seedbank):
         """Check if the seeds in the seedbank have the light and space conditions to germinate.
 
             Note: All seeds in the given seedbank are assumed to have the same conditions. Multiple calls to this method passing different seedbanks allow the representation of different requirements.
@@ -88,34 +87,31 @@ class Tree_Grid(Rectangular_Grid):
         Returns:
             est_seeds: a dictionary with patches as keys ('(x,y)') and number of established seeds as values.
         """
-        est_seeds={}
+        est_seeds = {}
 
         for ft in seedbank.keys():
-            h0=Tree.DERIVED_TREES[ft].h0
-            h1=Tree.DERIVED_TREES[ft].h1
-            min_light=Tree.DERIVED_TREES[ft].Iseed
-            seeds_to_keep=[]
+            h0 = Tree.DERIVED_TREES[ft].h0
+            h1 = Tree.DERIVED_TREES[ft].h1
+            min_light = Tree.DERIVED_TREES[ft].Iseed
+            seeds_to_keep = []
             for i in range(len(seedbank[ft])):
                 #print(i, len(seedbank[ft]), ft)
-                seed=seedbank[ft][i]
-                patch=(int(np.floor(seed[0])),int(np.floor(seed[1])))
-                if np.random.rand()>0.9995:
-                     seeds_to_keep.append(i)
-                     if not self.check_light(min_light,patch):
+                seed = seedbank[ft][i]
+                patch = (int(np.floor(seed[0])), int(np.floor(seed[1])))
+                if np.random.rand() > 0.9995:
+                    seeds_to_keep.append(i)
+                    if not self.check_light(min_light, patch):
                         seeds_to_keep.remove(i)
-                        #break
-                     elif not  self.check_CCA(h0,h1,patch):
+                        # break
+                    elif not self.check_CCA(h0, h1, patch):
                         seeds_to_keep.remove(i)
-                    
-            est_seeds[ft]=[seedbank[ft][s] for s in seeds_to_keep]
-            #est_seeds[ft]=seeds_to_keep    
 
+            est_seeds[ft] = [seedbank[ft][s] for s in seeds_to_keep]
+            # est_seeds[ft]=seeds_to_keep
 
         return est_seeds
 
-
-
-    def check_light(self,min_light, patch):
+    def check_light(self, min_light, patch):
         """ Check if the light intensity at ground level meets the needs for seed germination.
 
             Note: Ligth intensity is homogeneized at the patch level.
@@ -127,9 +123,9 @@ class Tree_Grid(Rectangular_Grid):
         Returns:
             Bool. True if light level is above minimum.
         """
-        return self.surface[self.dim_names["GroundLight"]][patch]>min_light
+        return self.surface[self.dim_names["GroundLight"]][patch] > min_light
 
-    def check_CCA(self,h0,h1,patch):
+    def check_CCA(self, h0, h1, patch):
         """ Check if the there is enough space forthe new plants to be established
 
             Note: Cummulative Crown Area is homogeneized at the patch level
@@ -142,13 +138,12 @@ class Tree_Grid(Rectangular_Grid):
         Returns:
             Bool. True if there is enough space.
         """
-        Hmin=h0*2**h1
-        l=int(np.floor(Hmin/self.delta_h))
-        if self.CCAs.get(patch)==None:
+        Hmin = h0 * 2**h1
+        l = int(np.floor(Hmin / self.delta_h))
+        if self.CCAs.get(patch) == None:
             return True
         else:
-            return self.CCAs[patch][l]<1.0
-
+            return self.CCAs[patch][l] < 1.0
 
     def ground_level_light(self):
         """Calculate the amount of light that reaches the ground.
@@ -159,11 +154,10 @@ class Tree_Grid(Rectangular_Grid):
             Returns:
                 None.
         """
-        self.surface[self.dim_names["GroundLight"]]=self.I0
+        self.surface[self.dim_names["GroundLight"]] = self.I0
         for patch in self.LAIs.keys():
-            self.surface[self.dim_names["GroundLight"]][patch]=self.I0*np.exp(-self.k*sum(self.LAIs[patch]))
-
-
+            self.surface[self.dim_names["GroundLight"]][patch] = self.I0 * \
+                np.exp(-self.k * sum(self.LAIs[patch]))
 
     def patch_based_LAI(self, trees_per_patch):
         """
@@ -177,19 +171,21 @@ class Tree_Grid(Rectangular_Grid):
             L_per_layer(dict): dictionary in which the keys are patch coordinates('(x,y)')-the same as the ones in trees_per_patch- and values are a list of the LAI per vertical layer
         """
 
-        L_per_layer={}
-        nlayers=int(np.ceil(np.ceil(Tree.MaxHeight())/self.delta_h))
+        L_per_layer = {}
+        nlayers = int(np.ceil(np.ceil(Tree.MaxHeight()) / self.delta_h))
         for k in trees_per_patch.keys():
-            L_per_layer[k]=[]
-            lower_limit=int(min([Tree.Instances.get(t).lmin for t in trees_per_patch[k]]))
-            upper_limit=int(max([Tree.Instances.get(t).lmax for t in trees_per_patch[k]]))
+            L_per_layer[k] = []
+            lower_limit = int(
+                min([Tree.Instances.get(t).lmin for t in trees_per_patch[k]]))
+            upper_limit = int(
+                max([Tree.Instances.get(t).lmax for t in trees_per_patch[k]]))
 
+            for i in range(nlayers + 1):
 
-            for i in range(nlayers+1):
+                L_per_layer[k].append((1 / self.patch_area) * sum([Tree.Instances.get(
+                    tree_id).L_mean for tree_id in trees_per_patch[k] if lower_limit <= i and upper_limit >= i]))
 
-                L_per_layer[k].append((1/self.patch_area)*sum([Tree.Instances.get(tree_id).L_mean for tree_id in trees_per_patch[k] if lower_limit<=i and upper_limit>=i]))
-
-                #upper and lower limits
+                # upper and lower limits
                 #Tree.Instances.get(tree_id).lmin<=i*self.delta_h and Tree.Instances.get(tree_id).lmax>=i*self.delta_h
         return L_per_layer
 
@@ -203,19 +199,22 @@ class Tree_Grid(Rectangular_Grid):
             L_per_layer(dict): dictionary in which the keys are the same as the ones in trees_per_patch and values are a list of the CCA per vertical layer
 
             """
-        L_per_layer={}
-        nlayers=int(np.ceil(np.ceil(Tree.MaxHeight())/self.delta_h))
+        L_per_layer = {}
+        nlayers = int(np.ceil(np.ceil(Tree.MaxHeight()) / self.delta_h))
         for k in trees_per_patch.keys():
-            L_per_layer[k]=[]
-            lower_limit=int(min([Tree.Instances.get(t).lmin for t in trees_per_patch[k]]))
-            upper_limit=int(max([Tree.Instances.get(t).lmax for t in trees_per_patch[k]]))
+            L_per_layer[k] = []
+            lower_limit = int(
+                min([Tree.Instances.get(t).lmin for t in trees_per_patch[k]]))
+            upper_limit = int(
+                max([Tree.Instances.get(t).lmax for t in trees_per_patch[k]]))
 
-            for i in range(nlayers+1):
+            for i in range(nlayers + 1):
 
-                L_per_layer[k].append((1/self.patch_area)*sum([Tree.Instances.get(tree_id).CA for tree_id in trees_per_patch[k] if lower_limit<=i and upper_limit>=i]))
+                L_per_layer[k].append((1 / self.patch_area) * sum([Tree.Instances.get(
+                    tree_id).CA for tree_id in trees_per_patch[k] if lower_limit <= i and upper_limit >= i]))
         return L_per_layer
 
-    def trees_in_patches(self,patches):
+    def trees_in_patches(self, patches):
         """ Make a list of tree ids and their respective positions.
 
         Args:
@@ -226,14 +225,13 @@ class Tree_Grid(Rectangular_Grid):
 
         """
 
-
-        #one list of ids for each patch
-        ids=[self.trees_per_patch[p] for p in patches]
-        #one single list of tuples with (id, pos)
-        id_pos=[(tree,Tree.Instances.get(tree).position) for patch in ids for tree in patch]
+        # one list of ids for each patch
+        ids = [self.trees_per_patch[p] for p in patches]
+        # one single list of tuples with (id, pos)
+        id_pos = [(tree, Tree.Instances.get(tree).position)
+                  for patch in ids for tree in patch]
 
         return id_pos
-
 
     def trees_per_age_groups(self, list_of_trees):
         """
@@ -242,15 +240,14 @@ class Tree_Grid(Rectangular_Grid):
         list_of_trees is a list with the id of trees.
         """
 
-        age_groups={}
+        age_groups = {}
         for t in list_of_trees:
-            if age_groups.get(Tree.Instances[t].age)==None:
-                age_groups[Tree.Instances[t].age]=[Tree.Instances[t].id]
-            else:age_groups[Tree.Instances[t].age].append(Tree.Instances[t].id)
+            if age_groups.get(Tree.Instances[t].age) == None:
+                age_groups[Tree.Instances[t].age] = [Tree.Instances[t].id]
+            else:
+                age_groups[Tree.Instances[t].age].append(Tree.Instances[t].id)
 
         return age_groups
-
-
 
     def trees_per_type(self, list_of_trees):
         """Organize trees by functional type.
@@ -263,14 +260,15 @@ class Tree_Grid(Rectangular_Grid):
             Types and values are lists containing the id of the respective trees.
         """
 
-        type_groups={}
+        type_groups = {}
         for t in list_of_trees:
-            if type_groups.get(Tree.Instances[t].Ftype)==None:
-                type_groups[Tree.Instances[t].Ftype]=[Tree.Instances[t].id]
-            else:type_groups[Tree.Instances[t].Ftype].append(Tree.Instances[t].id)
+            if type_groups.get(Tree.Instances[t].Ftype) == None:
+                type_groups[Tree.Instances[t].Ftype] = [Tree.Instances[t].id]
+            else:
+                type_groups[Tree.Instances[t].Ftype].append(
+                    Tree.Instances[t].id)
 
         return type_groups
-
 
     def cohorts(self, patches):
         """ Assemble cohorts based on tree type and age.
@@ -290,17 +288,14 @@ class Tree_Grid(Rectangular_Grid):
              {FunctionalType1:{AgeGroup1:[tree1,tree2,tree3...]}}
         """
 
-        trees_in_patch=self.trees_in_patches(patches)
-        list_of_trees=[i[0] for i in trees_in_patch]
-        FT=self.trees_per_type(list_of_trees)
-        cohorts={}
+        trees_in_patch = self.trees_in_patches(patches)
+        list_of_trees = [i[0] for i in trees_in_patch]
+        FT = self.trees_per_type(list_of_trees)
+        cohorts = {}
         for group in FT.keys():
-          cohorts[group]=self.trees_per_age_groups(FT[group])
+            cohorts[group] = self.trees_per_age_groups(FT[group])
 
         return cohorts
-
-
-
 
     def calculate_liana_coverage(self):
         """ Calculate the coverage(%) os lianas in on patch based on the previous coverage, temperature, humidity and wind speed
@@ -308,7 +303,7 @@ class Tree_Grid(Rectangular_Grid):
         Returns:
                 None
         """
-        return np.zeros((self.x_max,self.y_max))
+        return np.zeros((self.x_max, self.y_max))
 
     def calculate_liana_biomass(self):
         """
